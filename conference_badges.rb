ef56f7545ba09af307bb932ae640b2ed36d9a963
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-11-14 19:10:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -28,6 +28,7 @@ def assign_rooms(speakers_name)
 end
 
 def printer(speakers_name)
+  
   batch_badge_creator(speakers_name).each do
     puts batch_badge_creator  
   assign_rooms(speakers_name).each do  
